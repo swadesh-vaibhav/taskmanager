e57ad22ba56ed6bec695220435ce9ae312ef15df
swadesh-vaibhav <swadesh.vaibhav@gmail.com>
2024-02-09 00:26:34 +0530

    First Commit

---
+++ src/test/java/com/swadesh/taskmanager/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swadesh.taskmanager.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
